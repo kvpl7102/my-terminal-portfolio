f03dc44215e10d14b8d49f84f8fde005fdadda61
kinhvi_pl <kinhvi0710@gmail.com>
2025-07-05 00:58:05 -0400

    Update resume URL

--- src/components/commands/Welcome.tsx
+++ src/components/commands/Welcome.tsx
@@ -44,16 +44,16 @@ const Welcome: React.FC = () => {
         <div>Welcome to my terminal portfolio. For a list of available commands, type `<Cmd>help</Cmd>`.</div>
         <Seperator>--------</Seperator>
         <div>My {""}
-          <Link href="https://viphung-resume.tiiny.site">
+          <Link href="https://kvpl7102.github.io/my-resume/" target="_blank">
           resume
           </Link>
           {""} and my {""}
-          <Link href="https://github.com/kvpl7102">
+          <Link href="https://github.com/kvpl7102" target="_blank">
           github
           </Link>
         </div>
         <Seperator>--------</Seperator>
-        <div>This project was inspired by {""} <Link href="https://github.com/Cveinnt/LiveTerm">LiveTerm</Link> project and M4TT72's {""} <Link href="https://github.com/m4tt72/terminal">Terminal</Link></div>
+        <div>This project was inspired by {""} <Link href="https://github.com/Cveinnt/LiveTerm" target="_blank">LiveTerm</Link> project and M4TT72's {""} <Link href="https://github.com/m4tt72/terminal" target="_blank">Terminal</Link></div>
       </div>
       
     </HeroContainer>
